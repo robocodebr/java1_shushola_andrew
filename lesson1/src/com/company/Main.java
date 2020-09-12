72958896cdc7db90e1dbef126586bef626dcbbc4
natalia zhydejkina <Karina.Varlena@instantconnectnow.com>
2020-09-12 22:50:13 +0300

    lesson1

---
+++ lesson1/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    //    float f= 3.14f;
+    //    int integer = 1;
+    //    int i2;
+    //    i2 = 3;
+    //    char c = 'c';
+     //   long 1 = 1284938940;
+     //   byte b = 122;
+     //   boolean bb = true;
+     //   short s =1323;
+     //   String ss = "ggsngkti";
+
+     //   int sun = integer + i2;
+     //   int diff = integer - i2;
+     //   int ghj = integer * i2;
+
+      //  System.out.println(sun);
+        int vvs = 2005 + 10 + 30;
+        System.out.println.(vvs);
+    }
+}

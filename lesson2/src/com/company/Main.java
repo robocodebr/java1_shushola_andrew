ec4749f83185922acadee7fc5b515733a4820b4d
Priliv <stadnikd53@gmail.com>
2020-09-13 15:53:14 +0300

    lesson2

---
+++ lesson2/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+//        if (day>month) {
+//            system.out.println(day);
+//        }else if(years<month) {
+//            System.out.println 10;
+//        }else{System.out.println 2005;}
+//        String name = "Andrey";
+//        int n = name.length();
+//        String o = "Shyshola";
+//        int i = o.length();
+//        String a = "Michailovith";
+//        int p = o.length();
+//        if (n < i && i > p) {
+//            System.out.println("Shyshola");
+//        } else if (n > p && n > i) {
+//            System.out.println("Andrey");
+//        } else{
+//            System.out.println("Michailovith");
+//        }
+
+        Random random = new Random();
+        System.out.println(random.nextInt());
+        int r1 = random.nextInt();
+        int r2 = random.nextInt();
+        int r3 = random.nextInt();
+        if (r1 > r2);
+        if (r2<r3);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
